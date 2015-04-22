93944302f2bafe1e3891fe6336b362602cf10301
ettore <cipettaro@gmail.com>
2015-04-22 12:29:23 +0300

    print and generate key

--- src/crypto015/AES.java
+++ src/crypto015/AES.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package crypto015;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.security.AlgorithmParameters;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
@@ -20,6 +28,48 @@ import org.apache.commons.codec.binary.Base64;
  *
  * @author Ettore Ciprian <cipettaro@gmail.com>
  */
-public class AES {
-    
+public class AES { 
+   
+ private final int KEYSIZE = 256; //key size 
+ private final SecretKey SECRET_KEY = generateKey(); //final initial key
+    /**
+     * Generate a random secure Key with the KeyGenerator library standards
+     *
+     * @return SecretKey
+     */
+    public SecretKey generateKey() {
+        KeyGenerator keyGen = null;
+        try {
+            keyGen = KeyGenerator.getInstance("AES");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        keyGen.init(256); // for example
+        SecretKey secretKey = keyGen.generateKey();
+
+        return secretKey;
+    }
+
+    /**
+     * Print the random generated key to a file
+     *
+     * @param path where to save the key
+     * @throws java.io.FileNotFoundException
+     * @throws java.io.UnsupportedEncodingException
+     */
+    public void printKeytoFile(String path) throws FileNotFoundException, UnsupportedEncodingException {
+   
+        byte[] encoded = SECRET_KEY.getEncoded(); 
+        try (PrintWriter writer = new PrintWriter(path, "UTF-8")) {
+            System.out.println("256 bit key: ");
+            for (byte b : encoded){
+               writer.printf("%2X", b); 
+               System.out.printf("%2X", b);
+            }
+            writer.println();
+            writer.close();
+        }
+       
+    }
+
 }
--- src/crypto015/Main.java
+++ src/crypto015/Main.java
@@ -1,21 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package crypto015;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author ettore
+ * @author Ettore Ciprian <cipettaro@gmail.com>
  */
 public class Main {
-
+ private static final AES aes = new AES(); 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
+        if (args.length > 0 && args[0].equals("--print-key")){
+            if (!args[1].isEmpty()){
+                try {
+                    aes.printKeytoFile(args[1]);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    System.err.println("Please provide a valid path where to save the key");
+                } catch (UnsupportedEncodingException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            } else {
+                System.err.println("Please provide a path where to save the key");
+                System.exit(1);
+            }
+                
+            
+        }
     }
     
 }
