57d71eef3039666d15674969f6b9ea6b40f6625c
ettore <cipettaro@gmail.com>
2015-04-25 15:54:06 +0300

    load key method added

--- src/crypto015/AES.java
+++ src/crypto015/AES.java
@@ -1,39 +1,40 @@
 package crypto015;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import org.apache.commons.codec.binary.Base64;
 
 /**
  *
  * @author Ettore Ciprian <cipettaro@gmail.com>
  */
 public class AES {
-private int key_size;
-private SecretKey key;
 
-    
+    private int key_size;
+    private SecretKey key;
+
     public AES() {
         this.key_size = 256; //default set to 256 bit
         this.key = generateKey(); //Generate the key at start
     }
 
-    
     public int getKeySize() {
         return key_size;
     }
@@ -68,34 +69,109 @@ private SecretKey key;
         return keyGen.generateKey();
     }
 
+    public void encryptCBC(String message, SecretKey key) throws InvalidKeyException, InvalidAlgorithmParameterException {
+        byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchPaddingException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        cipher.init(Cipher.ENCRYPT_MODE, generateKey(), ivspec);
+
+        byte[] encrypted = null;
+        try {
+            encrypted = cipher.doFinal(message.getBytes());
+        } catch (IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Ciphertext: " + encodeHex(encrypted) + "\n");
+
+    }
+
     /**
      * Print the random generated key to a file
      *
-     * @param {String} path - Path where to save the key
-     * @throws java.io.FileNotFoundException
-     * @throws java.io.UnsupportedEncodingException
+     * @param path {String} - Path where to save the key
      */
-    public void printKeytoFile(String path) throws FileNotFoundException, UnsupportedEncodingException {
+    public void printKeytoFile(String path) {
 
         byte[] encoded = this.getKey().getEncoded();
         try (PrintWriter writer = new PrintWriter(path, "UTF-8")) {
-            System.out.println(this.getKeySize()+" bit key: ");
+            System.out.println(this.getKeySize() + " bit key: ");
             for (byte b : encoded) {
-                writer.printf("%2X", b);
+                writer.printf("%2X",b);
                 System.out.printf("%2X", b);
             }
             writer.println();
             writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Please provide a valid path where to save the key");
         }
 
     }
 
     /**
+     * Load the AES key of specified size from a file
      *
-     * @param {String} message - Message to be encrypted
+     * @param keyfile File meant for the key only
+     * @return
      */
-    public void encrypt(String message) {
-        //TODO
+    public SecretKey loadKeyfromFile(File keyfile) {
+        byte[] keybyte = new byte[(int) keyfile.length()];
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream(keyfile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            fin.read(keybyte);
+        } catch (IOException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            System.out.println("File path: "+keyfile.getCanonicalPath()+"\nI have got this key: " + encodeHex(keybyte));
+        } catch (IOException ex) {
+            Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new SecretKeySpec(keybyte, "AES");
+    }
+
+    /**
+     * *
+     * Encode to hex a byte array
+     *
+     * @param code - the byte array
+     * @return the resulting String
+     */
+    public String encodeHex(byte[] code) {
+        if (code == null || code.length == 0) {
+            return "";
+        }
+        StringBuilder output = new StringBuilder(code.length * 2);
+
+        for (byte b : code) {
+            if (b < 0x10) {
+                output.append("0");
+            }
+            output.append(Integer.toHexString(0xff & b));
+        }
+
+        return output.toString();
+
+    }
+
+    public void demoEncryption() {
+
+        String message = "This string contains a secret message.";
+        System.out.println("Plaintext: " + message + "\n");
+
     }
 
     public void decrypt(String encoded_message) {
--- src/crypto015/Main.java
+++ src/crypto015/Main.java
@@ -2,6 +2,7 @@ package crypto015;
 
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,58 +17,51 @@ public class Main {
      */
     public static void main(String[] args) {
         AES aes = new AES();
-        if (args.length > 0) {
-            switch (args[0]) {
+        switch (args[0]) {
 
-                case "--generate-key":
+            case "--generate-key":
 
-                    if (args.length < 2) {
-                        System.err.println("Please provide a path where to save the key");
+                if (args.length < 2) {
+                    System.err.println("Please provide a path where to save the key");
+                    System.exit(1);
+                } else {
+                    if (args.length < 3) {
+                        aes.printKeytoFile(args[1]);
+                    } else if (!args[2].matches("(128|192|256)")) {
+                        System.err.println("Please provide a valid key size (128, 192 or 256)");
                         System.exit(1);
                     } else {
-                        try {
-                            if (args.length < 3) {
-                                aes.printKeytoFile(args[1]);
-                            } else if (!args[2].matches("(128|192|256)")) {
-                                System.err.println("Please provide a valid key size (128, 192 or 256)");
-                                System.exit(1);
-                            } else {
-                                aes.setKeySize(Integer.parseInt(args[2]));
-                                aes.printKeytoFile(args[1]);
-                            }
-
-                        } catch (FileNotFoundException ex) {
-                            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                            System.err.println("Please provide a valid path where to save the key");
-                        } catch (UnsupportedEncodingException ex) {
-                            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-
+                        aes.setKeySize(Integer.parseInt(args[2]));
+                        aes.printKeytoFile(args[1]);
                     }
 
-                    break;
+                }
 
-                case "--encrypt-cbc":
-                    break;
-                case "--encrypt-cfb":
-                    break;
-
-                case "--decrypt":
-                    break;
-
-                default:
-                    System.err.println("Please provide a valid command!");
+                break;
+            case "--read-key":
+                if (args.length < 2) {
+                    System.err.println("Please provide the key path");
                     System.exit(1);
+                } else {
+                    aes.loadKeyfromFile(Paths.get(args[1]).toFile());
+                }
+                break;
+            case "--encrypt-cbc":
 
-            }
-            System.out.println();
-            System.out.println("EXECUTION COMPLETED");
-            System.exit(0);
+                break;
+            case "--encrypt-cfb":
+                break;
 
-        } else {
-            System.err.println("Please provide a valid command!");
-            System.exit(1);
+            case "--decrypt":
+                break;
+
+            default:
+                System.err.println("Please provide a valid command!");
+                System.exit(1);
+
         }
+        System.out.println();
+        System.exit(0);
 
     }
 }
